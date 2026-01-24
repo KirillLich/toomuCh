12cfa6511743782a765b42678f19b62d68f75032
Kirill_lovkov <lovkovk2@gmail.com>
2026-01-24 22:11:26 +0300

    Add config reading and main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,32 @@
 package main
 
-func main(){
-	
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/KirillLich/toomuCh/internal/config"
+	"github.com/go-chi/chi"
+)
+
+type HealthResponse struct {
+	Status string `json:"status"`
+}
+
+func main() {
+	cfg := config.SetConfig()
+	r := chi.NewRouter()
+	r.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(
+			HealthResponse{Status: fmt.Sprintf(
+				"Hello from main. Listen and serve at: %s:%d",
+				cfg.Server.Host,
+				cfg.Server.Port,
+			)})
+	})
+
+	fmt.Println(fmt.Sprintf("%s:%d", cfg.Server.Host, cfg.Server.Port))
+	fmt.Println(cfg)
+	http.ListenAndServe(fmt.Sprintf(":%d", cfg.Server.Port), r)
 }
---
+++ internal/config/config.go
@@ -0,0 +1,49 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Env    string
+	Server struct {
+		Host string
+		Port int
+	}
+	DB struct {
+		User     string
+		Password string
+		Name     string
+		Host     string
+		Port     int
+		SSLMode  string
+	}
+	App struct {
+		MessageMaxLen int
+	}
+}
+
+func SetConfig() *Config {
+	configPath := os.Getenv("CONFIG_PATH")
+	configName := os.Getenv("CONFIG_NAME")
+	viper.SetConfigName(configName)
+	viper.AddConfigPath(configPath)
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("fatal error config file: %w", err))
+	}
+
+	var cfg Config
+	err = viper.Unmarshal(&cfg)
+	if err != nil {
+		panic(fmt.Errorf("fatal error config file: %w", err))
+	}
+	return &cfg
+}
